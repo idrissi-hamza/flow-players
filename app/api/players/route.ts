ac13954a4e5597ac6008234e9b739620bd43b5c4
A-IdrissiHamza <a.idrissihamza@gmail.com>
2023-07-24 12:49:31 +0100

    check existing player and make endpoit reusable

--- app/api/players/route.ts
+++ app/api/players/route.ts
@@ -6,9 +6,28 @@ export const POST = async (req: Request, res: NextResponse) => {
   try {
     const { firstname, lastname, salary, goal, devise, pictureURL } =
       await req.json();
+
+    // Check if a player with the same firstname and lastname already exists
+    const existingPlayer = await prisma.player.findFirst({
+      where: {
+        firstname,
+        lastname,
+      },
+    });
+
+    if (existingPlayer) {
+      // Return an error response if the player already exists
+      return NextResponse.json(
+        { message: 'Player already exists', player: existingPlayer },
+        { status: 409 }
+      );
+    }
+
+    // If the player doesn't exist, create a new player
     const player = await prisma.player.create({
       data: { firstname, lastname, salary, goal, devise, pictureURL },
     });
+
     return NextResponse.json({ message: 'Success', player }, { status: 201 });
   } catch (err) {
     return NextResponse.json({ message: 'Error', err }, { status: 500 });
@@ -22,18 +41,31 @@ export const GET = async (req: NextRequest, res: NextResponse) => {
   const limit = url.searchParams.get('limit');
 
   // Check if skip and take are provided and parse them to integers
-  const parsedPage = parseInt(page || '', 10) || 0;
+  const parsedPage = parseInt(page || '', 10) || 1;
   const take = parseInt(limit || '', 10) || 10;
   const skip = (parsedPage - 1) * take;
-
+  
+// make it reusable for all and paginated result
   try {
-    const players = await prisma.player.findMany({
-      skip,
-      take,
-      orderBy: {
-        updatedAt: 'desc',
-      },
-    });
+    let players;
+
+    if (limit && page) {
+      // If both limit and page are provided, use pagination
+      players = await prisma.player.findMany({
+        skip,
+        take,
+        orderBy: {
+          updatedAt: 'desc',
+        },
+      });
+    } else {
+      // If either limit or page is missing, return all data
+      players = await prisma.player.findMany({
+        orderBy: {
+          updatedAt: 'desc',
+        },
+      });
+    }
 
     return NextResponse.json({ players, skip, take }, { status: 200 });
   } catch (err) {
